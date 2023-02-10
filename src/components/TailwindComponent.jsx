aad5753bb6d8a7aeed2c1acd9e11d9dc250e2837
junghae <najh66@gmail.com>
2023-02-10 17:43:04 +0900

    add

---
+++ src/components/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TailwindComponent() {
+  return (
+    <div>
+      <h1 className='text-2xl'>안녕!</h1>
+      <button className='bg-blue-300 rounded-md px-2'>Button</button>
+    </div>
+  );
+}
